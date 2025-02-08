1e2858cf561a73f395b4b95bf4ff353e16af66fa
João Matheus Oneta <joaomatheusoneta01@gmail.com>
2025-02-08 19:02:31 -0300

    Section #1: Introduction
    - Introduction to what is TypeScript, how to install Node.js, and how to install TypeScript globally via npm
    - First TypeScript script, with base knowledge about how to type function parameters

---
+++ 1-introduction/index.js
@@ -0,0 +1,7 @@
+var firstName = "João";
+var anotherName = 1;
+var x = true;
+var greeting = function (name) {
+    console.log("Hello, ".concat(name, "!"));
+};
+greeting(firstName);
---
+++ 1-introduction/index.ts
@@ -0,0 +1,9 @@
+const firstName = "João";
+const anotherName = 1;
+const x = true;
+
+const greeting = function(name: string){
+    console.log(`Hello, ${name}!`);
+}
+
+greeting(firstName);
